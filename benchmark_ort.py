4dce6381f10c0882f6d8639a448c228cf1b3fa51
David Schep <david.schep@eo.nl>
2024-06-15 21:48:16 +0200

    benchmark update

--- benchmark_ort.py
+++ benchmark_ort.py
@@ -3,6 +3,19 @@ import numpy as np
 import time
 
 yolon_path = "yolon_best.onnx"
+nanodet_path = "nanodet.onnx"
+int8_path = "yolon_int8.onnx"
+
+def run_bench(session_p, name):
+    random_image = np.random.rand(1, 3, 640, 640).astype(np.float32)
+    total = 0
+    for i in range(0,100):
+        start = time.perf_counter()
+        predictions = session_p.run(None, {name: random_image})
+        end = (time.perf_counter() - start) * 1000
+        total += end
+
+    print(total / 100, "ms average over 100 runs")
 
 random_image = np.random.rand(1, 3, 640, 640).astype(np.float32)
 
@@ -13,11 +26,18 @@ predictions = session.run(None, {'images': random_image})
 end = (time.perf_counter() - start) * 1000
 print("first one: ", end)
 
-total = 0
-for i in range(0,100):
-    start = time.perf_counter()
-    predictions = session.run(None, {'images': random_image})
-    end = (time.perf_counter() - start) * 1000
-    total += end
+run_bench(session, 'images')
 
-print(total / 100)
+del session
+
+session_nanodet = rt.InferenceSession(nanodet_path, providers=['CPUExecutionProvider'])
+
+print("nanodet:")
+run_bench(session_nanodet, 'data')
+
+del session_nanodet
+
+session_int8 = rt.InferenceSession(int8_path, providers=['CPUExecutionProvider'])
+
+print("int8 yolo:")
+run_bench(session_int8, 'images')
