84c5624c1b8758c03af3da045cd6d83a812d4232
David Schep <david.schep@eo.nl>
2024-06-16 18:52:36 +0200

    yolo bench update

--- benchmark_yolo.py
+++ benchmark_yolo.py
@@ -8,12 +8,14 @@ int8_dyn_path = "yolon_int8_dyn.onnx"
 int8_path = "yolon_int8.onnx"
 damoyolo_path = "damoyolo_Ns.onnx"
 
+model_int8_path_320 = 'yolon_int8_static_320.onnx'
+
 sess_options = rt.SessionOptions()
-sess_options.enable_profiling = True
+sess_options.enable_profiling = False
 sess_options.intra_op_num_threads = 1
 
 def run_bench(session_p, name, num=100):
-    random_image = np.random.rand(1, 3, 640, 640).astype(np.float32)
+    random_image = np.random.rand(1, 3, 320, 320).astype(np.float32)
     total = 0
     for i in range(0,num):
         start = time.perf_counter()
@@ -23,13 +25,13 @@ def run_bench(session_p, name, num=100):
 
     print(total / num, f"ms average over {num} runs")
 
-session_int8_dyn = rt.InferenceSession(int8_dyn_path, providers=['CPUExecutionProvider'], sess_options=sess_options)
+session_int8_static_320 = rt.InferenceSession(model_int8_path_320, providers=['CPUExecutionProvider'], sess_options=sess_options)
 
-print("int8 dynamic yolo:")
+print("int8 static 320 yolo:")
 
 while (True):
     start = time.time()
-    run_bench(session_int8_dyn, 'images', 1)
+    run_bench(session_int8_static_320, 'images', 1)
     end = (time.time() - start)
     time.sleep(1-end)
 
